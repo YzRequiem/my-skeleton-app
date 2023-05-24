377320de555819e76e1719ca020353249bc7aded
leo <mxmleosallier@gmail.com>
2023-05-24 12:39:09 +0200

    api active

--- src/routes/api/posts/+server.js
+++ src/routes/api/posts/+server.js
@@ -16,15 +16,16 @@ export const POST = async ({request}) => {
     const body = await request.json()
      console.log(body);
 
-    //  if (body) {
-    //   const newValue = await prisma.mesure.create(
-    //     {
-    //       data: body
-    //     }
-    //   );
-    //   console.log('coucou' + newValue);
-    //   newValue
-    //  }
+     
+      if (body) {
+      const newValue = await prisma.mesure.create(
+        {
+          data: body
+        }
+      );
+      console.log('coucou' + newValue);
+      newValue
+     }
 
     return new Response(
     JSON.stringify({message: 'Ta requette à bien été reçue michel '}),
